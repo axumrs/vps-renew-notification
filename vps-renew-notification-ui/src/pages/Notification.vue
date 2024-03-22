dda73d8b009e70addcabb3e441d071be339c5565
axumrs <rolson867@gmail.com>
2024-03-23 05:05:18 +0800

    UI/通知设置

--- vps-renew-notification-ui/src/pages/Notification.vue
+++ vps-renew-notification-ui/src/pages/Notification.vue
@@ -2,12 +2,24 @@
 import PageTitle from "@/components/PageTitle.vue";
 import Input from "@/components/Input.vue";
 import Form from "@/components/Form.vue";
+import Button from "@/components/Button.vue";
+
+const testHandler = () => {
+  window.confirm("我们将发送一条测试信息到TG机器人，是否继续？");
+};
 </script>
 
 <template>
   <PageTitle>通知设置</PageTitle>
 
   <Form>
-    <Input label="TG机器人Token" />
+    <div class="flex justify-start items-end gap-x-2">
+      <div class="grow">
+        <Input label="TG机器人Token" />
+      </div>
+      <div class="shrink-0 relative">
+        <Button theme="info" @click="testHandler">测试</Button>
+      </div>
+    </div>
   </Form>
 </template>
